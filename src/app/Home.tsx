d35d2ac7c118e622f7e395b1a1f2154eeb4e5595
luishenriquetp <luishenriquetp@gmail.com>
2025-03-09 20:32:43 -0300

    Initial commit

---
+++ src/app/Home.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div
+      className="relative w-full h-screen bg-cover bg-center"
+      style={{
+        backgroundImage: "url('https://lorempixel.com/1920/1080/nature/')" // Lorem Pixel image URL
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div> {/* Dark overlay */}
+      <div className="relative z-10 flex justify-center items-center w-full h-full text-white">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold mb-4">Welcome to Our Website</h1>
+          <p className="text-xl mb-8">Explore amazing features and get started today!</p>
+          <a
+            href="#"
+            className="bg-blue-500 hover:bg-blue-700 text-white py-3 px-8 rounded-lg text-lg transition duration-300"
+          >
+            Get Started
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/Navbar.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="fixed top-0 left-0 w-full flex items-center justify-center bg-gray-900 p-4 z-10">
+      <ul className="flex items-center justify-between w-full max-w-7xl px-8">
+        <li><Link href="/home" className=" hover:underline">Home</Link></li>
+        <li><Link href="/about" className=" hover:underline">About</Link></li>
+        <li><Link href="/services" className=" hover:underline">Services</Link></li>
+        <li><Link href="/" className=" hover:underline mx-auto">
+        <Image
+              className="dark:invert"
+              src="/globe.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+        </Link></li>
+        <li><Link href="/blog" className=" hover:underline">Blog</Link></li>
+        <li><Link href="/contact" className=" hover:underline">Contact</Link></li>
+        <li><Link href="/portfolio" className=" hover:underline">Portfolio</Link></li>
+      </ul>
+    </nav>
+  );
+}
