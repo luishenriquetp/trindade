08b69fdae0da41b81646957a12abb027da31be01
luishenriquetp <luishenriquetp@gmail.com>
2025-04-10 21:01:48 -0300

    2 columns section implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       </div>
     </div>
     <div
-      className="relative w-full h-screen bg-cover bg-center"
+      className="relative w-full h-150 bg-cover bg-center"
     >
       <div className="absolute inset-0 bg-[#ebebeb]"></div> {/* Dark overlay */}
       <div className="relative z-10 flex justify-center items-center w-full h-full">
@@ -40,23 +40,37 @@ export default function Home() {
         </div>
       </div>
     </div>
-    <div
-      className="relative w-full h-screen bg-cover bg-center"
-    >
-      <div className="absolute inset-0"></div> {/* Dark overlay */}
-      <div className="relative z-10 flex justify-center items-center w-full h-full">
-        <div className="text-center">
-          <h1 className="text-5xl font-bold mb-4 text-[#494949]">Welcome to Our Third</h1>
-          <p className="text-xl mb-8">Explore amazing features and get started today!</p>
-          <a
-            href="#"
-            className="font-sans uppercase bg-[#f1345d] hover:bg-[#f1345d] text-white py-3 px-8 font-semibold text-xs transition duration-300"
-          >
-            Get Started
-          </a>
-        </div>
+    <div className="relative w-full h-80 bg-cover bg-center">
+  <div className="absolute inset-0"></div>
+  <div className="relative z-10 flex justify-center items-center w-full h-full">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-30 text-center">
+      
+      <div>
+        <h1 className="text-5xl font-bold mb-4 text-[#494949]">Welcome to Our Third</h1>
+        <p className="text-xl mb-8">Explore amazing features and get started today!</p>
+        <a
+          href="#"
+          className="font-sans uppercase bg-[#f1345d] hover:bg-[#f1345d] text-white py-3 px-8 font-semibold text-xs transition duration-300"
+        >
+          Get Started
+        </a>
       </div>
+
+      <div>
+        <h1 className="text-5xl font-bold mb-4 text-[#494949]">Welcome to Our Third</h1>
+        <p className="text-xl mb-8">Explore amazing features and get started today!</p>
+        <a
+          href="#"
+          className="font-sans uppercase bg-[#f1345d] hover:bg-[#f1345d] text-white py-3 px-8 font-semibold text-xs transition duration-300"
+        >
+          Get Started
+        </a>
+      </div>
+
     </div>
+  </div>
+</div>
+
     
     </>
     
